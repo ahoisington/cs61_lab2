8c6d85e10bf0dbe3c433948489bce8d58bede0b4
acaciah <acaciah@acacias-mbp.wireless.dartmouth.edu>
2017-05-09 03:41:43 -0400

    lol check for validity of registering editor or author or reviewer. there's an OutOfBoundsException error on line 232 that i know how to fix.  created functions editor_register, author_register, reviewer_register, isEmailAddress, isInteger. we can certainly change formatting.  validity checks for this lab are going to be a bitch lol. cool cool acacia out

--- frontend.java
+++ frontend.java
@@ -4,12 +4,15 @@
   connecting to a MySQL database and executing a query
  */
 import java.sql.*;
+import java.io.*;
+import java.util.*;
 
 public class frontend {
     public static final String SERVER   = "jdbc:mysql://sunapee.cs.dartmouth.edu/";
     public static final String USERNAME = "acaciah";
     public static final String PASSWORD = "Password1";
     public static final String DATABASE = "acaciah_db";
+
 
     public static void main(String[] args) {
 		Connection con = null;
@@ -18,60 +21,296 @@ public class frontend {
 		int numColumns = 0;
 		String query = null;
 
-		// attempt to connect to db
+		// CONNECT TO DB
 		try {
-		    // load mysql driver
+		    // LOAD MYSQL DRIVER
 		    Class.forName("com.mysql.jdbc.Driver").newInstance();
 
-		    // initialize connection
+		    // INIT CONNECTION
 		    con = DriverManager.getConnection(SERVER+DATABASE, USERNAME, PASSWORD);
 		    System.out.println("Connection established.");
 
+		    //TAKE IN USeR INPUT
+	        BufferedReader buffReader = null;
+	        try {
+	            buffReader = new BufferedReader(new InputStreamReader(System.in));
+	            while (true) {
+
+	                System.out.print("Enter your request: "); 	// ASK USER FOR INPUT
+	                String input = buffReader.readLine();
+
+	                if ("q".equals(input)) {	// USER WANTS TO EXIT PROGRAM
+	                    System.out.println("Exit!");
+	                    buffReader.close();
+	               		break; //leave buffReader loop. then prog will close connection to MySQL server
+	                } 
+
+	               	String[] request = input.split(" ");
+
+	               	if (request[0].equals("register")){
+	               		String[] req_to_check = Arrays.copyOfRange(request,2,request.length);
+	               		System.out.println("req_to_check is " + Arrays.toString(req_to_check));
+	               		if (request[1].equals("author") && author_register(req_to_check)){
+	               			//run register sql query to insert
+	               		} else if (request[1].equals("reviewer") && reviewer_register(req_to_check)){
+	               			//run register sql query to insert	
+	               		} else if (request[1].equals("editor") && editor_register(req_to_check)){
+	               			//run register sql query to insert
+	               		} else {
+	               			System.err.println("Input error: Make sure your request to register follows the correct format. Read documentation or input -h or --help for guidance.");
+	               			System.exit(1);
+	               		}
+	               	}
 
 
 
 
 
-		    // initialize a query statement
-		    stmt = con.createStatement();
 
-		    // query db and save results
-		    res = stmt.executeQuery(query);
-		    System.out.format("Query executed: '%s'\n\nResults:\n", QUERY);
-		    
-		    // the result set contains metadata
-		    numColumns = res.getMetaData().getColumnCount();
 
-		    // print table header
-		    for(int i = 1; i <= numColumns; i++) {
-			System.out.format("%-12s", res.getMetaData().getColumnName(i));
-		    }
-		    System.out.println("\n--------------------------------------------");
 
-		    // iterate through results
-		    while(res.next()) {
-			for(int i = 1; i <= numColumns; i++) {
-			    System.out.format("%-12s", res.getObject(i));
-			}
-			System.out.println("");
-		    }
-		} catch (SQLException e ) {          // catch SQL errors
-		    System.err.format("SQL Error: %s", e.getMessage());
-		} catch (Exception e) {              // anything else
+				    //////////////////////////////////////////////////////////////
+
+	                query = "SELECT * FROM person;";
+
+				    // INITIALIZE QUERY STATEMENT
+				    stmt = con.createStatement();
+
+				    // QUERY DB. SAVE RESULTS
+				    res = stmt.executeQuery(query);
+				    System.out.format("Query executed: '%s'\n\nResults:\n", query);
+				    // System.out.println("results " +res.getObject(1));
+				    // RESULT SET CONTAINS META DATA
+				    numColumns = res.getMetaData().getColumnCount();
+
+				    // PRINT RESULTS OF QUERY
+				    for(int i = 1; i <= numColumns; i++) { 			//PRINT HEADER
+						System.out.format("%-12s", res.getMetaData().getColumnName(i));
+				    }
+				    System.out.println("\n--------------------------------------------");
+				    while(res.next()) { 		// ITERATE THROUGH RESULTS TO PRINT THEM
+						for(int i = 1; i <= numColumns; i++) {
+						    System.out.format("%-12s", res.getObject(i));
+						}
+						System.out.println("");
+				    }
+
+				    //////////////////////////////////////////////////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	            }
+
+	        } catch (IOException e) { 
+	            e.printStackTrace();
+	        } finally { //close buffReader
+	            if (buffReader != null) {
+	                try {
+	                    buffReader.close();
+	                } catch (IOException e) {
+	                    e.printStackTrace();
+	                }
+	            }
+	        } //end of bufferedReader
+		} catch (SQLException e ) {    // catch SQL errors
+		    System.err.format("SQL Error: %s\n", e.getMessage());
+		} catch (Exception e) {  		// anything else
 		    e.printStackTrace();
-		} finally {
-		    // cleanup
+		} finally {		// clean up
 		    try {
-			con.close();
-			stmt.close();
-			res.close();
-			System.out.print("\nConnection terminated.");
+				con.close();
+				stmt.close();
+				res.close();
+				System.out.print("\nConnection terminated.\n");
 		    } catch (Exception e) { /* ignore cleanup errors */ }
 		}
     }
 
+	/*
+     *
+     * author_register checks if num of args and format of register-author request is correct. returns boolean.	
+     * true if format is:	 <fname> <lname> <email> <address> <affiliation>
+     *
+     */
+    public static boolean  author_register (String[] req){
+    	if (req.length == 4){
+			if (isEmailAddress(req[2]) && isInteger(req[4])){  //email && affiliation code
+				return true;
+			} else {
+				System.err.println("Input error: incorrect formatting of values");
+				System.exit(1);  //HALP ERROR OCCURS SO FUNC SHOULD END EVERYTHING. IS THIS CORRECT THING TO DO? EVEN THOU
+								//IT DOESNT CLOSE THE BUFFREADER OR SQLDRIVER
+				return false; //HALP ERROR OCCURS SO FUNC SHOULD END EVERYTHING 
+			}
+    	} else {
+    		System.err.println("Input error: invalid number of input vals");
+    		System.exit(1);
+    	}
+    	return false;
+    } 
 
 
 
 
+// //LOL THIS CHECKS IF THE VALUES IN A STRING ARRAY ARE STRINGS. *facepalm* (unless elisabeth thinks this is fine haha)
+//     /*
+//      *
+//      * author_register checks if format of register-author request is correct. returns boolean.	
+//      * true if format is:	 <fname> <lname> <email> <address> <affiliation>
+//      *
+//      */
+//     public static boolean  author_register (String[] reg){
+//     	if (reg.length == 4){
+// 			if (reg[0].isString() && //fname
+// 				reg[1].isString() && //lname
+// 				isEmailAddress(reg[2]) && //email
+// 				reg[3].isString() &&	//address
+// 				reg[4].isInteger()){ //affiliation
+// 				return true;
+// 			} else {
+// 				System.err.println("Input error: invalid number of input vals");
+// 				System.exit(1);  //HALP ERROR OCCURS SO FUNC SHOULD END EVERYTHING. IS THIS CORRECT THING TO DO? EVEN THOU
+// 								//IT DOESNT CLOSE THE BUFFREADER OR SQLDRIVER
+// 				return false; //HALP ERROR OCCURS SO FUNC SHOULD END EVERYTHING 
+// 			}
+//     	} else {
+//     		System.err.println("Input error: invalid number of input vals");
+//     		System.exit(1);
+//     	}
+//     } 
+    	
+        /*
+   //   *
+   //   * reviewer_register checks if format of register-author request is correct. returns boolean.	
+   //   * true if format is:	 <fname> <lname> <email> <address>
+   //   * reg[0] is author, editor or reviewer. 
+   //   *
+   //   */
+   //  public static boolean  reviewer_register (String[] reg){
+   //  	if (reg.length >= 4 && reg.length <= 7){
+			// if (reg[0].isString() && //fname
+			// 	reg[1].isString() && //lname
+			// 	isEmailAddress(reg[2]) && //email
+			// 	reg[3].isInteger()){ //affiliationCode
+			// 	return true;
+			// } else {
+			// 	System.err.println("Input error: invalid number of input vals");
+			// 	System.exit(1);  //HALP ERROR OCCURS SO FUNC SHOULD END EVERYTHING. IS THIS CORRECT THING TO DO? EVEN THOU
+			// 					//IT DOESNT CLOSE THE BUFFREADER OR SQLDRIVER
+			// 	return false; //HALP ERROR OCCURS SO FUNC SHOULD END EVERYTHING 
+			// }
+   //  	} else {
+   //  		System.err.println("Input error: invalid number of input vals");
+   //  		System.exit(1);
+   //  	}
+   //  } 
+
+
+    /*
+    *
+    * reviewer_register checks if num of args and format of register-reviewer request is correct. returns boolean.	
+    * correct format is:	 <fname> <lname> <email> <affiliationCode> <RICode1> <RICode2 (optional)> <RICode3 (optional)>
+    *
+    */
+    public static boolean reviewer_register (String[] req){
+    	if (req.length >= 4 && req.length<= 7){
+			if (isEmailAddress(req[2]) && isInteger(req[4]) && isInteger(req[5]) && isInteger(req[6]) && isInteger(req[7]) ){  //email && affiliation code and RICodes
+				return true;
+			} else {
+				System.err.println("Input error: incorrect formatting of values");
+				System.exit(1);  //HALP ERROR OCCURS SO FUNC SHOULD END EVERYTHING. IS THIS CORRECT THING TO DO? EVEN THOU
+								//IT DOESNT CLOSE THE BUFFREADER OR SQLDRIVER
+				return false; //HALP ERROR OCCURS SO FUNC SHOULD END EVERYTHING 
+			}
+    	} else {
+    		System.err.println("Input error: invalid number of input vals");
+    		System.exit(1);
+    	}
+    	return false;
+    } 
+
+
+    /*
+    *
+    * editor_register checks if num of args and format of register-editor request is correct. returns boolean.	
+    * correct format is:	 <fname> <lname> <email> 
+    *
+    */
+    public static boolean editor_register (String[] req){
+    	if (req.length == 3){
+			if (isEmailAddress(req[2])){  //email && affiliation code and RICodes
+				return true;
+			} else {
+				System.err.println("Input error: incorrect formatting of input vals.");
+				System.exit(1);  
+			}
+    	} else {
+    		System.err.println("Input error: invalid number of input vals");
+    		System.exit(1);
+    	}
+    	return false;
+    } 
+    	
+
+   	/*
+   	 *
+   	 * copied from internet. that okay? my inner good student feels wary.
+   	 * http://stackoverflow.com/questions/624581/what-is-the-best-java-email-address-validation-method
+   	 */
+    public static boolean isEmailAddress(String email) {
+           String ePattern = "^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@((\\[[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\.[0-9]{1,3}\\])|(([a-zA-Z\\-0-9]+\\.)+[a-zA-Z]{2,}))$";
+           java.util.regex.Pattern p = java.util.regex.Pattern.compile(ePattern);
+           java.util.regex.Matcher m = p.matcher(email);
+           return m.matches();
+    }
+
+
+    /*
+    *
+    * checks if val is integer. haha another one from internet aah i hope this is okay lolol
+    * http://stackoverflow.com/questions/8336607/how-to-check-if-the-value-is-integer-in-java
+    */
+    public static boolean isInteger(String str) {
+	    try {
+	        Integer.parseInt(str);
+	        return true;
+	    } catch (NumberFormatException nfe) {
+	        return false;
+	    }
+	}
+    	
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
 }
